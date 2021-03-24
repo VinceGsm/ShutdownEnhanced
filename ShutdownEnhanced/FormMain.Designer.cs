4b363b9e008e83d816412de0afabf7b67a3fd5ad
VinceGusmini <vincent.gusmini.sio@gmail.com>
2021-03-24 23:12:26 +0100

    V.1.0.0 setup

--- ShutdownEnhanced/FormMain.Designer.cs
+++ ShutdownEnhanced/FormMain.Designer.cs
@@ -158,6 +158,7 @@ namespace ShutdownEnhanced
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Shutdown Enhanced";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
